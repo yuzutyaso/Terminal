bfd8b10b8327f6afae39ae139bee28ac2fa9b227
yuzutyaso <f.kokori85@gmail.com>
2025-08-11 18:10:34 +0900

    Update server.js

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ const express = require('express');
 const { createClient } = require('@supabase/supabase-js');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const path = require('path'); // pathモジュールを追加
+const path = require('path');
 
 const app = express();
 
 // --- ここにご自身のSupabaseの情報を直接記述してください ---
-const SUPABASE_URL = 'https://zlbfsyixwncpictplcvk.supabase.co';
+const SUPABASE_URL = 'https://zlbfsyixwncpictplcvk.supabase.co'; 
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpsYmZzeWl4d25jcGljdHBsY3ZrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ4MDkwOTcsImV4cCI6MjA3MDM4NTA5N30.AGoC-NxJTJPKnyU1pb2ICONGYd4b5EzdW6nkcOyXdao';
 // --------------------------------------------------------
 
@@ -16,11 +16,8 @@ const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 app.use(cors());
 app.use(bodyParser.json());
 
-// 静的ファイル（publicフォルダ内のCSS, JSなど）を配信
 app.use(express.static('public'));
 
-// ホーム画面のルートを追加
-// '/'へのGETリクエストが来たときに、public/index.htmlを返す
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
